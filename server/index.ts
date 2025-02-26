c636377e3ea651327eab111b9efdf15d921ba0c1
Jacob Meyer <jacobpmeyer87@gmail.com>
2025-02-25 17:21:43 -0800

    Change the production path

--- server/index.ts
+++ server/index.ts
@@ -28,10 +28,10 @@ app.use('/api/users', userRoutes);
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
   });
 }
 
